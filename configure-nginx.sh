e24adcf9f9f7bae947985a78eded8c0b5286e73f
Mike Nichols <nichols.mike.s@gmail.com>
2014-10-14 17:45:21 -0600

    remove unused bits

--- configure-nginx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#mkdir /usr/local/etc/nginx/sites-enabled
-#mkdir /usr/local/etc/nginx/sites-available
-rm -rf /usr/local/etc/nginx/sites-available/consistency
-rm -rf /usr/local/etc/nginx/sites-enabled/consistency
-cp ./nginx-config /usr/local/etc/nginx/sites-available
-ln -s /usr/local/etc/nginx/sites-available/consistency /usr/local/etc/nginx/sites-enabled
-nginx -t && nginx -s reload
